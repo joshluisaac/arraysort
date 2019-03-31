d7756a6ae0b73b68fbe8705c4c0e3f6787195440
Josh Uzo <nwankwo.joshua@gmail.com>
2019-04-01 03:45:51 +1100

    Updated unit tests

--- src/test/java/unittests/InsertionSortTest.java
+++ src/test/java/unittests/InsertionSortTest.java
@@ -1,7 +1,38 @@
 package unittests;
 
+import com.sorting.components.IMergeSort;
+import com.sorting.components.InsertionSort;
+import com.sorting.components.InstrumentationSort;
+import com.sorting.components.MergeSort;
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class InsertionSortTest {
 
 
+    /* dependencies */
+    InstrumentationSort instrumentationSort;
+
+    @Before
+    public void run_once_per_test() {
+        instrumentationSort = new InsertionSort();
+    }
+
+
+    @Test
+    public void sorting_an_integer_array_should_return_ordered_collection() {
+
+        /* given the following array */
+        int[] arr = new int[]{7, 3, 9, 1, 2, 4, 5, 6, 82, 0, -1};
+
+        /* when i sort it */
+        int[] result = instrumentationSort.sort(arr);
+
+        /* i expect [-1, 0, 1, 2, 3, 4, 5, 6, 7, 9, 82] after sorting */
+        Assert.assertThat(result, CoreMatchers.is(new int[]{-1, 0, 1, 2, 3, 4, 5, 6, 7, 9, 82}));
+
+    }
 
 }
--- src/test/java/unittests/SortServiceTest.java
+++ src/test/java/unittests/SortServiceTest.java
@@ -1,4 +1,33 @@
 package unittests;
 
+import com.sorting.components.MergeSort;
+import com.sorting.model.SortResponse;
+import com.sorting.services.SortService;
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class SortServiceTest {
+
+    /* dependencies */
+    SortService service;
+
+    @Before
+    public void run_once_per_test() {
+        service = new SortService(new MergeSort());
+    }
+
+    @Test
+    public void executing_sort_service_returns_sort_response(){
+        /* given the following array */
+        int[] arr = new int[]{7, 3, 9, 1, 2, 4, 5, 6};
+
+        /* when i execute sort */
+        SortResponse response = service.execute(arr);
+
+        /* i expect [1, 2, 4, 5, 6, 7, 9] after sorting */
+        Assert.assertThat(response.getSortResult(), CoreMatchers.is("[1, 2, 3, 4, 5, 6, 7, 9]"));
+    }
+
 }
