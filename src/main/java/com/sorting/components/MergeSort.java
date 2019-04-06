a2b79cf560ba88fd4488cc1be38a2c72cfb6f35c
Josh Uzo <nwankwo.joshua@gmail.com>
2019-04-06 23:00:16 +1100

    Updated left and right pointer

--- src/main/java/com/sorting/components/MergeSort.java
+++ src/main/java/com/sorting/components/MergeSort.java
@@ -6,14 +6,18 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
-
 /**
- * An implementation of {@link IMergeSort} that contains methods for performing merge sort.
+ * An implementation of {@link IMergeSort} that contains methods for performing
+ * merge sort.
  *
- * <p>The algorithm uses a divide and conquer technique which was invented by John von Neumann in 1945.
+ * <p>
+ * The algorithm uses a divide and conquer technique which was invented by John
+ * von Neumann in 1945.
  * (<a href="https://en.wikipedia.org/wiki/Merge_sort">MergeSort</a>)
  *
- * <p> The algorithm offers <strong>O(n log(n))</strong> performance in all cases (worst,average and best case)
+ * <p>
+ * The algorithm offers <strong>O(n log(n))</strong> performance in all cases
+ * (worst,average and best case)
  *
  * @author Joshua Nwankwo
  * @version 1.0
@@ -27,23 +31,23 @@ public class MergeSort extends AbstractSort implements IMergeSort {
     private static final Logger LOG = LoggerFactory.getLogger(MergeSort.class);
     private int count = -1;
 
-
     /**
      * Calculates and returns an array midpoint.
      *
      * @param arr the input array
      * @return the array midpoint
      */
-    public int calculateMidPoint(int[] arr){
+    public int calculateMidPoint(int[] arr) {
         return arr.length / 2;
     }
 
     /**
-     * Populates the left fragment of the array with left elements from the parent array.
+     * Populates the left fragment of the array with left elements from the parent
+     * array.
      *
      * @param midPoint the midpoint
-     * @param left left array fragment
-     * @param arr the parent array
+     * @param left     left array fragment
+     * @param arr      the parent array
      * @return resultant left array
      */
     public int[] loadLeftArray(int midPoint, int[] left, int[] arr) {
@@ -54,10 +58,11 @@ public class MergeSort extends AbstractSort implements IMergeSort {
     }
 
     /**
-     * Populates the right fragment of the array with right elements from the parent array.
+     * Populates the right fragment of the array with right elements from the parent
+     * array.
      *
      * @param right right array fragment
-     * @param arr the parent array
+     * @param arr   the parent array
      * @return resultant right array
      */
     public int[] loadRightArray(int[] right, int[] arr) {
@@ -68,13 +73,14 @@ public class MergeSort extends AbstractSort implements IMergeSort {
     }
 
     /**
-     * Will construct the right array based on the midpoint and the parent array.
-     * If the length of the parent array is divisible by 2 without a remainder
-     * This means the length is an even number and the initial array can be divided into equal parts.
-     * However, if it is not divisible by 2 then it is an odd number and an else construction rule is applied.
+     * Will construct the right array based on the midpoint and the parent array. If
+     * the length of the parent array is divisible by 2 without a remainder This
+     * means the length is an even number and the initial array can be divided into
+     * equal parts. However, if it is not divisible by 2 then it is an odd number
+     * and an else construction rule is applied.
      *
      * @param midPoint the midpoint
-     * @param arr the parent array
+     * @param arr      the parent array
      * @return the right array
      */
     public int[] constructRightArray(int midPoint, int[] arr) {
@@ -85,12 +91,13 @@ public class MergeSort extends AbstractSort implements IMergeSort {
         }
     }
 
-
     /**
-     * This is a recursive split operation which will split the left side of the array until it gets to the base case
-     * and can no longer be splitted any further.
-     * It then propagates to the right array fragment and does the same splitting operation.
+     * This is a recursive split operation which will split the left side of the
+     * array until it gets to the base case and can no longer be splitted any
+     * further. It then propagates to the right array fragment and does the same
+     * splitting operation.
      * <p>
+     * 
      * @param arr the array to be sorted
      * @return the sorted array
      */
@@ -98,7 +105,8 @@ public class MergeSort extends AbstractSort implements IMergeSort {
     @Override
     public int[] sort(int[] arr) {
         /* base case condition */
-        if (arr.length <= 1) return arr;
+        if (arr.length <= 1)
+            return arr;
 
         int midPoint = calculateMidPoint(arr);
         int[] left = new int[midPoint];
@@ -115,36 +123,41 @@ public class MergeSort extends AbstractSort implements IMergeSort {
      * Will merge the left and right sides of the array.
      *
      *
-     * @param leftArr the left array fragment
+     * @param leftArr  the left array fragment
      * @param rightArr the right array fragment
      * @return the result of merging both arrays
      */
     public int[] merge(int[] leftArr, int[] rightArr) {
         int[] result = new int[leftArr.length + rightArr.length];
 
-        int leftIndex = 0;
-        int rightIndex = 0;
+        // pointer to the current position in left array
+        int leftPtr = 0;
+
+        // pointer to the current position in right array
+        int rightPtr = 0;
+
+        // pointer to the current position in the sorted array
         int resultIndex = 0;
 
-        while (leftIndex < leftArr.length || rightIndex < rightArr.length) {
-            if (leftIndex < leftArr.length && rightIndex < rightArr.length) {
-                if (leftArr[leftIndex] < rightArr[rightIndex]) {
-                    result[resultIndex] = leftArr[leftIndex];
+        while (leftPtr < leftArr.length || rightPtr < rightArr.length) {
+            if (leftPtr < leftArr.length && rightPtr < rightArr.length) {
+                if (leftArr[leftPtr] < rightArr[rightPtr]) {
+                    result[resultIndex] = leftArr[leftPtr];
                     resultIndex++;
-                    leftIndex++;
+                    leftPtr++;
                 } else {
-                    result[resultIndex] = rightArr[rightIndex];
+                    result[resultIndex] = rightArr[rightPtr];
                     resultIndex++;
-                    rightIndex++;
+                    rightPtr++;
                 }
-            } else if (leftIndex < leftArr.length) {
-                result[resultIndex] = leftArr[leftIndex];
+            } else if (leftPtr < leftArr.length) {
+                result[resultIndex] = leftArr[leftPtr];
                 resultIndex++;
-                leftIndex++;
-            } else if (rightIndex < rightArr.length) {
-                result[resultIndex] = rightArr[rightIndex];
+                leftPtr++;
+            } else if (rightPtr < rightArr.length) {
+                result[resultIndex] = rightArr[rightPtr];
                 resultIndex++;
-                rightIndex++;
+                rightPtr++;
             }
         }
         count++;
@@ -152,14 +165,14 @@ public class MergeSort extends AbstractSort implements IMergeSort {
     }
 
     /**
-     * A forwarding method which overrides the behaviour of the
-     * parent abstract method in an attempt to set the number of iterations.
+     * A forwarding method which overrides the behaviour of the parent abstract
+     * method in an attempt to set the number of iterations.
      *
      * @param arr raw input array
      * @return returns {@link SortResponse} which contains a bunch of properties
      */
     @Override
-    public SortResponse execute(int[] arr){
+    public SortResponse execute(int[] arr) {
         SortResponse response = super.execute(arr);
         response.setCount(count);
         return response;
